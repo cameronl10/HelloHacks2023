9c9030ba73b27dfb992a6d04bbe61a3cbfa55348
cameron lee <cameronblazes@gmail.com>
2023-10-01 19:34:15 -0700

    last commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
 
 mongoose.connect(process.env.DATABASE_URI, {
     useNewUrlParser: true
